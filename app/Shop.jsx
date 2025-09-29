d8ad7591dd29b4b0887e17a5c768ab1153097e23
Valeria Kartasheva <valeriakartaseva39@gmail.com>
2025-09-29 20:25:37 +0300

    Подключение иконочного шрифта

---
+++ app/Shop.jsx
@@ -0,0 +1,9 @@
+import styles from './shop.module.css';
+
+export const Shop = () => {
+	return (
+		<div className={styles.appColumn}>
+			<i class="fa fa-free-code-camp" aria-hidden="true"></i>
+		</div>
+	);
+};
