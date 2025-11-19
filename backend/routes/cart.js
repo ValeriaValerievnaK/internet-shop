f71b793db3606a154b98b7fbe57b899374a459f9
Valeria Kartasheva <valeriakartaseva39@gmail.com>
2025-11-19 22:20:28 +0300

    создание контроллера и роута корзины

---
+++ backend/routes/cart.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const {
+  getCart,
+  addProductToCart,
+  updateCartItem,
+  deleteCartItem,
+} = require("../controllers/cart");
+const authenticated = require("../middlewares/authenticated");
+const hasRole = require("../middlewares/hasRole");
+const ROLES = require("../constants/roles");
+
+const router = express.Router({ mergeParams: true });
+
+
+router.get("/", authenticated, async (req, res) => {
+  try {
+   const cartItems = await getCart(req.query.userId);
+    res.send({ data: cartItems });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+router.post("/", authenticated, hasRole([ROLES.ADMIN, ROLES.BUYER]), async (req, res) => {
+  try {
+    const newCartItem = await addProductToCart({
+      product_id: req.body.productId,
+      user_id: req.body.userId,
+      image_url: req.body.imageUrl,
+      title: req.body.title,
+      price: req.body.price,
+      count: req.body.count || 1,
+    });
+
+    res.send({ data: newCartItem });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+
+router.patch("/:id", authenticated, hasRole([ROLES.ADMIN, ROLES.BUYER]), async (req, res) => {
+  try {
+    const updatedCartItem = await updateCartItem(req.params.id, {
+      count: req.body.count,
+      price: req.body.price,
+    });
+
+    res.send({ data: updatedCartItem });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+
+router.delete("/:id", authenticated, hasRole([ROLES.ADMIN, ROLES.BUYER]), async (req, res) => {
+  try {
+    await deleteCartItem(req.params.id);
+    res.send({ error: null });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,6 @@ const router = express.Router({ mergeParams: true });
 router.use("/", require("./auth"));
 router.use("/products", require("./product"));
 router.use("/users", require("./user"));
+router.use("/cart", require("./cart"));
 
 module.exports = router;
