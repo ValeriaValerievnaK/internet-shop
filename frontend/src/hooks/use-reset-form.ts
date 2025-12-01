952a3dc56cda7dc63bcb9ff5cfe3faa704491ea9
Valeria Kartasheva <valeriakartaseva39@gmail.com>
2025-12-01 21:20:55 +0300

    переход на TS hooks

--- frontend/src/hooks/use-reset-form.ts
+++ frontend/src/hooks/use-reset-form.ts
@@ -1,8 +1,16 @@
 import { useEffect } from 'react';
+import type { UseFormReset } from 'react-hook-form';
 import { useStore } from 'react-redux';
+import type { TRootState } from '../store';
 
-export const useResetForm = (reset) => {
-	const store = useStore();
+interface IResetForm {
+	login: string;
+	password: string;
+	passwordchek: string;
+}
+
+export const useResetForm = (reset: UseFormReset<IResetForm>) => {
+	const store = useStore<TRootState>();
 
 	useEffect(() => {
 		let currentWasLogout = store.getState().app.wasLogout;
